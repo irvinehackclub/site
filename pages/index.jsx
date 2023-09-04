0de0789e924ee202ee81b4e516febd702f43e48a
Manu G <manusvathgurudath@gmail.com>
2023-09-04 18:58:00 +0000

    asdf

--- pages/index.jsx
+++ pages/index.jsx
@@ -40,18 +40,48 @@ export default function Page() {
       >
         <Box
           sx={{
-            px: 5,
-            py: 6,
-            background: 'red',
-            borderRadius: '28px'
+            borderRadius: '28px',
+            position: 'relative',
+            overflow: 'hidden'
           }}
         >
-          <Heading as="h2" color="border">
-            CALLING ALL STUDENTS TO
-          </Heading>
-          <Heading variant="title" color="white" as="h1">
-            Make cool stuff
-          </Heading>
+          <img
+            src="/cover.jpg"
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              zIndex: -1,
+              objectFit: 'cover'
+            }}
+          />
+          <Box
+            sx={{
+              top: 0,
+              left: 0,
+              width: '100%',
+              px: [3, 4, 5],
+              py: 6,
+              height: '100%',
+              background:
+                'linear-gradient(90deg, rgba(255,255,255,0.9) 60%, transparent)'
+            }}
+          >
+            <Heading variant="eyebrow">CALLING ALL STUDENTS&nbsp;TO</Heading>
+            <Heading color="#375F3D" variant="ultratitle" as="h1">
+              Make cool stuff
+            </Heading>
+            <Heading
+              color="#375F3D"
+              sx={{ opacity: 0.6 }}
+              variant="subtitle"
+              as="h1"
+            >
+              Regardless of coding experience
+            </Heading>
+          </Box>
         </Box>
       </Box>
     </>
