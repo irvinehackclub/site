00db40135dcaa71753d2966e41e12b90f166ee4f
Yoda <yoda@yodacode.xyz>
2023-09-12 10:18:04 -0700

    interestform

--- next.config.js
+++ next.config.js
@@ -1,2 +1,8 @@
 const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
-module.exports = withMDX({ pageExtensions: ['js', 'jsx', 'mdx'] })
+module.exports = withMDX({ pageExtensions: ['js', 'jsx', 'mdx'], redirects: [
+    {
+        destination: 'https://docs.google.com/forms/d/e/1FAIpQLSec08cro79830ch3YEX6zHUhyOPD5fHHLJfU068XFLIGEwVOA/viewform',
+        permanent: true,
+        source: '/forms/interest'
+    }
+] })
