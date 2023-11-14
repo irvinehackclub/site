e0b1bf7e014cd787f07b14c83198f1d1cbe30f92
Yoda <yoda@yodacode.xyz>
2023-11-14 12:15:53 -0800

    changes

--- pages/clicker.js
+++ pages/clicker.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-const url = 'https://firewalledreplit.com/@IanMadden2/ClickerGame-Template?forkRepl=15afb76e-b5f2-4a51-a57a-1c72ddb68a83&forkContext=coverPage&redirecting=1';
+const url = 'https://firewalledreplit.com/@IanMadden2/ClickerGame-Template';
 
 export default function Clicker () {
     useEffect(() => {
