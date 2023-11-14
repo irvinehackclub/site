f5b2a4c9d225660ba1ae513599681d41c25008d2
Yoda <yoda@yodacode.xyz>
2023-11-13 23:52:55 -0800

    changes

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = withMDX({ pageExtensions: ['js', 'jsx', 'mdx'], redirects: [
         source: '/forms/interest'
     },
     {
-        destination: 'https://firewalledreplit.com/@IanMadden2/ClickerGame-Template',
+        destination: 'https://firewalledreplit.com/@IanMadden2/ClickerGame-Template?forkRepl=15afb76e-b5f2-4a51-a57a-1c72ddb68a83&forkContext=coverPage&redirecting=1',
         permanent: true,
         source: '/clicker'
     }
