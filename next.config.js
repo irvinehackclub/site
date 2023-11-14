5379406abb9ad1ad29e1c4adcfb57aae055a3b28
Yoda <yoda@yodacode.xyz>
2023-11-14 10:02:46 -0800

    changes

--- next.config.js
+++ next.config.js
@@ -9,10 +9,5 @@ module.exports = withMDX({ pageExtensions: ['js', 'jsx', 'mdx'], redirects: [
         destination: 'https://firewalledreplit.com/@IanMadden2/ClickerGame-Template?forkRepl=15afb76e-b5f2-4a51-a57a-1c72ddb68a83&forkContext=coverPage&redirecting=1',
         permanent: true,
         source: '/clicker'
-    },
-    {
-        destination: 'https://docs.google.com/presentation/d/1SVJ4tC4Pl4FtxugCypry8w4Ey9-zVIpCWLeTkQjkqt8/edit?usp=sharing',
-        permanent: true,
-        source: '/clickerslides'
     }
 ] })
