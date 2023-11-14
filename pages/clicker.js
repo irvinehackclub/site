379e94556ae51b4215490bdabe143216ca85898d
Yoda <yoda@yodacode.xyz>
2023-11-14 10:06:22 -0800

    changes

--- pages/clicker.js
+++ pages/clicker.js
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 
+const url = 'https://firewalledreplit.com/@IanMadden2/ClickerGame-Template?forkRepl=15afb76e-b5f2-4a51-a57a-1c72ddb68a83&forkContext=coverPage&redirecting=1';
+
 export default function Clicker () {
     useEffect(() => {
-        window.location.replace('https://firewalledreplit.com/@IanMadden2/ClickerGame-Template?forkRepl=15afb76e-b5f2-4a51-a57a-1c72ddb68a83&forkContext=coverPage&redirecting=1');
+        window.location.replace(url);
     }, []);
 
     return (
-        <p>Redirecting...</p>
+        <p>Redirecting...<br />If you aren't redirected, click <a href={url}>here</a>.</p>
     );
 }
