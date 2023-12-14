e85f3ffaf9222842117d4bc6d8de2438045adfb1
Yoda <yoda@yodacode.xyz>
2023-12-14 08:09:27 -0800

    AI

--- pages/api/ai.js
+++ pages/api/ai.js
@@ -44,11 +44,17 @@ export default async function handler (req, res) {
                 response: errorResponse + ' (Code A)'
             });
         }
-        const completion= await openai.chat.completions.create({
-            messages: [{ role: "system", content: "You are a helpful assistant." }],
+
+        const completion = await openai.chat.completions.create({
+            messages: [{ role: "system", content: prompt }],
             model: "gpt-3.5-turbo",
-          })
+        });
+
         const response = completion.choices[0].message.content;
+
+        res.status(200).json({
+            response
+        });
 
         await fetch("https://hooks.slack.com/triggers/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", {
             method: "POST",
@@ -64,9 +70,7 @@ export default async function handler (req, res) {
             })
         });
 
-        return res.status(200).json({
-            response
-        });
+        return;
     } catch (err) {
         console.error(err);
 
